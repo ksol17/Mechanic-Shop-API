4844a4b756b642e0437129d8a5afa2e17506a511
ksol17 <karla.pauta001@gmail.com>
2025-11-12 17:08:49 -0500

    prepped for deployment

--- config.py
+++ config.py
@@ -1,15 +1,19 @@
+
 import os
 
-
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    CACHE_TYPE = 'SimpleCache'
-    SECRET_KEY = os.getenv("SECRET_KEY")
-
-class DevelopmentConfig(Config):
+class DevelopmentConfig:
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Preciosa16!@localhost/mechanic_shop_db'
     DEBUG = True
+    CACHE_TYPE = 'SimpleCache'  
 
-class ProductionConfig(Config):
-    DEBUG = False
+class TestingConfig:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
+    DEBUG = True
+    CACHE_TYPE = 'SimpleCache'
+
+
+
+class ProductionConfig:
+    SQLALCHEMY_DATABASE_URI= os.environ.get('SQLALCHEMY_DATABASE_URI')
+    CACHE_TYPE = 'SimpleCache'
+    SECRET_KEY= 'supersecretkey12345'
--- flask_app.py
+++ flask_app.py
@@ -1,21 +1,10 @@
-from dotenv import load_dotenv
-import os
-from app import create_app, db
-from config import ProductionConfig, DevelopmentConfig
+from app import create_app
+from app.models import db
 
 
-# Load .env for local development
-load_dotenv()
+app = create_app('ProductionConfig')
 
-# Pick config based on FLASK_ENV
-env = os.getenv("FLASK_ENV", "development").lower()
-config = ProductionConfig if env == "production" else DevelopmentConfig
-
-# Create Flask app
-app = create_app(config)
-
-# Optional: create tables if they don’t exist
 with app.app_context():
-    db.create_all()
 
-#gunicorn flask_app:app
+    db.create_all()  # Create database tables if they don't exist
+
