3efce6b3cb8c2f924a9b1bb618a98680e2bb923f
ksol17 <karla.pauta001@gmail.com>
2025-11-12 12:16:44 -0500

    prepped

--- flask_app.py
+++ flask_app.py
@@ -1,25 +1,14 @@
 from dotenv import load_dotenv
 import os
 from app import create_app, db
-from config import DevelopmentConfig, ProductionConfig
+from config import ProductionConfig
 
-# Choose env file
-env_file = ".env.production" if os.getenv("FLASK_ENV") == "production" else ".env.development"
-load_dotenv(env_file)
+load_dotenv(".env.production")  
 
-# Determine environment
-env = os.getenv("FLASK_ENV", "development").lower()
+app = create_app(ProductionConfig)
 
-# Pick config class
-config = ProductionConfig if env == "production" else DevelopmentConfig
-
-# Create Flask app
-app = create_app(config)
-
-# Initialize database tables (optional)
 with app.app_context():
     db.create_all()
-
 
 
 #gunicorn flask_app:app
