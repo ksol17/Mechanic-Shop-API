c6d2ff51c95d608281ada655b3ec9a03dd645919
ksol17 <karla.pauta001@gmail.com>
2025-11-12 11:41:44 -0500

    prepped for deployment

--- config.py
+++ config.py
@@ -6,19 +6,17 @@ import os
 
 
 class DevelopmentConfig:
-    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Preciosa16!@localhost/mechanic_shop_db'
     DEBUG = True
-    CACHE_TYPE = 'SimpleCache'  
+    SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://root:Preciosa16!@localhost/mechanic_shop_db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CACHE_TYPE = 'SimpleCache'
 
 class TestingConfig:
-
     SQLALCHEMY_DATABASE_URI = 'sqlite:///testing.db'
     DEBUG = True
-    CACHE_TYPE = 'SimpleCache'  
-    
+    CACHE_TYPE = 'SimpleCache'
 
 class ProductionConfig:
-   
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///prod.db')
-    CACHE_TYPE = 'SimpleCache'  # Use SimpleCache for production
-
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CACHE_TYPE = 'SimpleCache'
--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,29 @@
-from app import create_app
-from app.models import db
-from config import ProductionConfig
+from dotenv import load_dotenv
+import os
+from app import create_app, db
+from config import  DevelopmentConfig, TestingConfig, ProductionConfig
 
 
+# Load environment variables from .env file
+load_dotenv()
 
-app = create_app('ProductionConfig')
+# Determine the environment
+env = os.getenv('FLASK_ENV', 'development').lower()
 
+# Pick configuration based on environment
+if env == 'production':
+    config = ProductionConfig
+elif env == 'testing':
+    config = TestingConfig
+else:
+    config = DevelopmentConfig
+
+# Create Flask app using selected configuration
+app = create_app(config)
+
+# Initialize database tables if needed(Production environment should handle migrations separately)
 with app.app_context():
     db.create_all()
-
-
+    
 
 #gunicorn flask_app:app
