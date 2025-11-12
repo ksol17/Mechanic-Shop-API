d31d8fb76c4f3832782d88369461125fba98bc0d
ksol17 <karla.pauta001@gmail.com>
2025-11-12 10:54:21 -0500

    ready for deployment

--- config.py
+++ config.py
@@ -19,7 +19,6 @@ class TestingConfig:
 
 class ProductionConfig:
    
-    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI', 'sqlite:///prod.db')
     CACHE_TYPE = 'SimpleCache'  # Use SimpleCache for production
 
